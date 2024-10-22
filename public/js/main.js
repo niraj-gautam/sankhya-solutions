2f1f9d5e6c6e6825099538c3a29473ee920a2b12
niraj-gautam <nirajgautamith@gmail.com>
2024-10-22 08:32:54 +0545

    channged device

--- public/js/main.js
+++ public/js/main.js
@@ -58,17 +58,17 @@
     }
 
     // Header scroll
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $(".header").addClass("header-scrolled");
-        } else {
-            $(".header").removeClass("header-scrolled");
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 100) {
+    //         $(".header").addClass("header-scrolled");
+    //     } else {
+    //         $(".header").removeClass("header-scrolled");
+    //     }
+    // });
 
-    if ($(window).scrollTop() > 100) {
-        $(".header").addClass("header-scrolled");
-    }
+    // if ($(window).scrollTop() > 100) {
+    //     $(".header").addClass("header-scrolled");
+    // }
 
     // Carousel
     var carousel = $(".carousel");
@@ -116,15 +116,15 @@
         allowPageScroll: "vertical",
     });
 
-    // Skills section
-    $(".skills").waypoint(
-        function () {
-            $(".progress .progress-bar").each(function () {
-                $(this).css("width", $(this).attr("aria-valuenow") + "%");
-            });
-        },
-        { offset: "80%" }
-    );
+    // // Skills section
+    // $(".skills").waypoint(
+    //     function () {
+    //         $(".progress .progress-bar").each(function () {
+    //             $(this).css("width", $(this).attr("aria-valuenow") + "%");
+    //         });
+    //     },
+    //     { offset: "80%" }
+    // );
 
     // jQuery counterUp
     $('[data-toggle="counter-up"]').counterUp({
