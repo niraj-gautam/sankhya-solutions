bdc2b1ef7c7c1a430b826c110799317618c1e0be
niraj-gautam <nirajgautamith@gmail.com>
2024-10-23 11:17:41 +0545

    new system

--- src/js/main.js
+++ src/js/main.js
@@ -116,15 +116,15 @@
         allowPageScroll: "vertical",
     });
 
-    // Skills section
-    $(".skills").waypoint(
-        function () {
-            $(".progress .progress-bar").each(function () {
-                $(this).css("width", $(this).attr("aria-valuenow") + "%");
-            });
-        },
-        { offset: "80%" }
-    );
+    // // Skills section
+    // $(".skills").waypoint(
+    //     function () {
+    //         $(".progress .progress-bar").each(function () {
+    //             $(this).css("width", $(this).attr("aria-valuenow") + "%");
+    //         });
+    //     },
+    //     { offset: "80%" }
+    // );
 
     // jQuery counterUp
     $('[data-toggle="counter-up"]').counterUp({
