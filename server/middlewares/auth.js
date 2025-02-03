c753005f13b7f4c72812303ddcb14bd5822f0999
niraj-gautam <nirajgautamith@gmail.com>
2025-02-03 13:03:54 +0545

    change backend to strapi cms

--- server/middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if (!req.session.user) {
-        return res
-            .status(401)
-            .json({ success: false, message: "Unauthorized" });
-    }
-    next();
-};
